cf01f8e123a0741034d433ad182971ef408ba2f1
denvilse1 <119152406+denvilse1@users.noreply.github.com>
2024-12-04 14:01:40 +0530

    python logic

---
+++ python-logic/pattern.py
@@ -0,0 +1,11 @@
+for i in range(0,4):
+    for j in range(0,4):
+        print('*',end=" ")
+    print()
+
+print()
+
+for i in range(0,4):
+    for j in range(0,i+1):
+        print("* ",end="")
+    print()
