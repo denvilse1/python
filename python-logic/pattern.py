8d7cf6a805acf85df04c048428fd37ed3134dce4
denvilse1 <119152406+denvilse1@users.noreply.github.com>
2024-12-04 14:06:05 +0530

    python logic

--- python-logic/pattern.py
+++ python-logic/pattern.py
@@ -9,3 +9,14 @@ for i in range(0,4):
     for j in range(0,i+1):
         print("* ",end="")
     print()
+
+
+for i in range(0,4):
+    for j in range(0,i+1):
+        print(i,end="")
+    print()
+
+for i in range(0,4):
+    for j in range(0,i+1):
+        print(j+1,end="")
+    print()
