91253cdb2e662632bb9b9753da53d281532271a9
denvilse1 <119152406+denvilse1@users.noreply.github.com>
2024-12-05 11:05:15 +0530

    day 3

---
+++ python-logic/strings.py
@@ -0,0 +1,20 @@
+a="This is Elon Musk"
+print(a)
+print(a[1])
+print("------------------")
+
+# printing characters in strings 
+# way one 
+for i in a:
+    print(i,end="==")
+print()
+print("++++++++++++++")
+
+for i in range(0,len(a)):
+    print(a[i],end="++")
+
+print()
+
+print("This is Ram's pen")
+print('Some once said "if you are failing to prepare, then you are preparing to fail"')
+print("some once said \"God favors the brave\" : let's see")
