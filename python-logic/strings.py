280d4db0e3a5c52716c9190032df1efdb1463a75
denvilse1 <119152406+denvilse1@users.noreply.github.com>
2024-12-06 10:36:33 +0530

    python day 2

--- python-logic/strings.py
+++ python-logic/strings.py
@@ -18,3 +18,32 @@ print()
 print("This is Ram's pen")
 print('Some once said "if you are failing to prepare, then you are preparing to fail"')
 print("some once said \"God favors the brave\" : let's see")
+
+print("comparing two strings")
+a=input("Enter your first string ")
+b=input("Enter your second string")
+
+if a==b:
+    print(f"both are same .i.e. is {a} and {b}")
+else:
+    print(f"{a} and {b} they  are not equal")
+
+
+using ord() and chr()
+a='d'
+b=ord(a)
+print(b)
+b=chr(b)
+print(b)
+
+using .split()
+
+a="my name is Elon Musk"
+print(a.split())
+
+a=" "
+print(ord(a))
+
+# analysing white spacing using the ord function
+for i in a:
+    print(ord(i),end="----")
